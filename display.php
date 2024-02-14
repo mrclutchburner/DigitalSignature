aea4cfe23c1b4f28f9f71f8a6fb1fd0583d0f942
Marcellana, Francis John A <85853642+mrclutchburner@users.noreply.github.com>
2024-02-14 12:01:20 +0800

    search bar updated

--- display.php
+++ display.php
@@ -1,9 +1,48 @@
+<?php
+include 'connect_test_db.php';
+$searchErr = '';
+$image_details='';
+if(isset($_POST['save']))
+{
+	if(!empty($_POST['search']))
+	{
+		$search = $_POST['search'];
+		$stmt = $con->prepare("select * from employee_sign where name like '%$search%' or address like '%$search%'");
+		$stmt->execute();
+		$image_details = $stmt->fetchAll(PDO::FETCH_ASSOC);
+		//print_r($employee_details);
+		
+	}
+	else
+	{
+		$searchErr = "Please enter the information";
+	}
+   
+}
+
+?>
 <html>
  <head>
  <title>AGMA 2024</title>
  </head>
  <body>
- 		
+            <form class="form-horizontal" action="#" method="post">
+                <div class="row">
+                    <div class="form-group">
+                        <label class="control-label col-sm-4" for="email"><b>Search Consumer Attendance:</b>:</label>
+                        <div class="col-sm-4">
+                        <input type="text" class="form-control" name="search" placeholder="search here">
+                        </div>
+                        <div class="col-sm-2">
+                        <button type="submit" name="save" class="btn btn-success btn-sm">Submit</button>
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <span class="error" style="color:red;">* <?php echo $searchErr;?></span>
+                    </div>
+                    
+                </div>
+            </form>
 		
         <img src="./upload/pdf.png" class="pdf_icon" onclick="generatePDF();"/>
 		<a href="export.php" onClick=rec();>
@@ -36,22 +75,28 @@
 									</tr>
 								</thead>
 							
-								
-								<?php
-								$conn = mysqli_connect("localhost", "root", "", "sign");
-								$image_details  = mysqli_query($conn, "SELECT * FROM employee_sign");
-								while ($row = mysqli_fetch_array($image_details)) {    
-									echo "<tr>
-										<td>".$row['name']."</td>
-										<td>".$row['address']."</td>
-										<td>".$row['acctnum']."</td>
-										<td>".$row['contact']."</td>
-										<td><img src='".$row['signature_img']."' height= 65px width= auto></td>
-									</tr>"; 
-									
-								}     
+                                <?php
 
-								?>
+                                if(!$image_details)
+                                {
+                                    echo '<td>NO Data Found</td>';
+                                }else{
+                                    foreach($image_details as $key=>$row)
+                                    {
+                                      
+                                        echo "
+                                        <tr>
+                                        <td>".$row['name']."</td>
+                                        <td>".$row['address']."</td>
+                                        <td>".$row['acctnum']."</td>
+                                        <td>".$row['contact']."</td>
+                                        <td><img src='".$row['signature_img']."'></td>
+                                        </tr>
+                                        ";
+                                        
+                                    }
+                                }
+                                ?>
 								
 								</div>
                         
